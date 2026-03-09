2e70e62eaa95dfbab02406f5ba919acfbb8e426f
jatinlakhani0596 <jatinlakhani0596@gmail.com>
2026-03-09 12:08:41 +0530

    w

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // For Vercel deployment
+  base: "./", // IMPORTANT FOR VERCEL
 
   resolve: {
     alias: {
